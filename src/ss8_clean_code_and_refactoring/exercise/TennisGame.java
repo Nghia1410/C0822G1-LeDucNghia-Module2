730c162d81311dc995293a6a2b0d7bd37d01ebf9
Ducnghia1410 <nghia14102003@gmail.com>
2022-10-16 22:51:59 +0700

    nop bai tap

--- src/ss8_clean_code_and_refactoring/exercise/TennisGame.java
+++ src/ss8_clean_code_and_refactoring/exercise/TennisGame.java
@@ -1,61 +1,78 @@
 package ss8_clean_code_and_refactoring.exercise;
 
 public class TennisGame {
+    public static final int LOVE = 0;
+    public static final int FIFTEEN = 1;
+    public static final int THIRTY = 2;
+    public static final int FOURTY = 3;
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(int firstPlayerScore, int secondPlayerScore) {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+        int tempScore = 0;
+        if (firstPlayerScore == secondPlayerScore) {
+            return result(firstPlayerScore);
+        }
 
+        if (firstPlayerScore >= 4 || secondPlayerScore >= 4) {
+            return win(firstPlayerScore, secondPlayerScore);
+        }
+
+        return currentScore(firstPlayerScore, secondPlayerScore, score);
+    }
+
+    public static String result(int firstPlayerScore) {
+        switch (firstPlayerScore) {
+            case LOVE:
+                return "Love-All";
+            case FIFTEEN:
+                return "Fifteen-All";
+            case THIRTY:
+                return "Thirty-All";
+            case FOURTY:
+                return "Forty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    public static String win(int firstPlayerScore, int secondPlayerScore) {
+        int minusResult = firstPlayerScore - secondPlayerScore;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        }
+
+        if (minusResult == -1) {
+            return "Advantage player2";
+        }
+
+        if (minusResult >= 2) {
+            return "Win for player1";
+        }
+
+        return "Win for player2";
+    }
+
+    public static String currentScore(int firstPlayerScore, int secondPlayerScore, String score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = firstPlayerScore;
+            else {
+                score += "-";
+                tempScore = secondPlayerScore;
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+            switch (tempScore) {
+                case LOVE:
+                    score += "Love";
+                    break;
+                case FIFTEEN:
+                    score += "Fifteen";
+                    break;
+                case THIRTY:
+                    score += "Thirty";
+                    break;
+                case FOURTY:
+                    score += "Forty";
+                    break;
             }
         }
         return score;
