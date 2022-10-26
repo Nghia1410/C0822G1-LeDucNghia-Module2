2c293cb4ae7bcb47950c1fe2243c0f3fb8ca8e6e
Ducnghia1410 <nghia14102003@gmail.com>
2022-10-26 10:34:34 +0700

    nop bai tap

---
+++ src/ss15_xu_li_ngoai_le_va_debug/exercise/IllegalTriangleException.java
@@ -0,0 +1,7 @@
+package ss15_xu_li_ngoai_le_va_debug.exercise;
+
+public class IllegalTriangleException extends Exception{
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+}
---
+++ src/ss15_xu_li_ngoai_le_va_debug/exercise/Triangle.java
@@ -0,0 +1,38 @@
+package ss15_xu_li_ngoai_le_va_debug.exercise;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        do {
+            double size1, size2, size3;
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Nhập độ dài cạnh thứ nhất: ");
+                size1 = Double.parseDouble(scanner.nextLine());
+                System.out.print("Nhập độ dài cạnh thứ hai ");
+                size2 = Double.parseDouble(scanner.nextLine());
+                System.out.print("Nhập độ dài cạnh thứ ba: ");
+                size3 = Double.parseDouble(scanner.nextLine());
+                triangleCheck(size1, size2, size3);
+                if (size1 + size2 >= size3 || size3 + size2 >= size1 || size3 + size1 >= size2) {
+                    break;
+                }
+            } catch (IllegalTriangleException e) {
+                System.out.println(e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("Nhập Số");
+            }
+        } while (true);
+    }
+
+    public static void triangleCheck(double size1, double size2, double size3) throws IllegalTriangleException {
+        if (size1 <= 0 || size3 <= 0 || size2 <= 0) {
+            throw new IllegalTriangleException("Tổng hai cạnh phải lớn hơn cạnh còn lại. Vui lòng nhập lại: ");
+        } else {
+            System.out.println("Tam giác có cạnh x = " + size1);
+            System.out.println("Tam giác có cạnh y = " + size2);
+            System.out.println("Tam giác có cạnh z = " + size3);
+        }
+    }
+}
